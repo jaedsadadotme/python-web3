4601e219a3e38b717076888cd8d43be46c70f9a0
Jaedsada <jaedsadadotme@Jaedsadas-MacBook-Pro.local>
2021-07-05 23:18:04 +0700

    first commit

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from web3 import Web3
 web3    = Web3(Web3.HTTPProvider('https://bsc-dataseed.binance.org/'))
 wallet = "<token_address>"
 
-
 with open('contract/Exchange.json', 'r') as exchage_definition:
  exchage = json.load(exchage_definition)
 
@@ -23,36 +22,36 @@ with open('contract/DOP.json', 'r') as abi_definition:
 address     = abi['Dopple']['Pool']['DiamondHands']['address']
 abi         = abi['Dopple']['Pool']['DiamondHands']['ABI']
 
-BUSD    = web3.toChecksumAddress(token['BUSD'])
-DOP     = web3.toChecksumAddress(token['DOP'])
-Twin     = web3.toChecksumAddress(token['TWIN'])
+BUSD        = web3.toChecksumAddress(token['BUSD'])
+DOP         = web3.toChecksumAddress(token['DOP'])
+Twin        = web3.toChecksumAddress(token['TWIN'])
 
 factory_address = exchage["Twindex"]["Factory"]
-factory_abi = factory["Factory"]
+factory_abi     = factory["Factory"]
 factory_pair    = factory["Pairs"]
 
 
 def getDopPrice(factory_address,factory_abi,factory_pair,InputTokenAddr,OutputTokenAddr) :
-    web3 = Web3(Web3.HTTPProvider('https://bsc-dataseed.binance.org/'))
-    pancake_factory_address = web3.toChecksumAddress(factory_address)
-    contract        = web3.eth.contract(address=pancake_factory_address, abi=factory_abi)
+    factory_address = web3.toChecksumAddress(factory_address)
+    contract        = web3.eth.contract(address=factory_address, abi=factory_abi)
+
     pair_address    = contract.functions.getPair(InputTokenAddr,OutputTokenAddr).call()
     pair1           = web3.eth.contract(abi=factory_pair, address=pair_address)
-    reserves = pair1.functions.getReserves().call()
-    reserve0 = reserves[0]
-    reserve1 = reserves[1]
+
+    reserves        = pair1.functions.getReserves().call()
+    reserve0        = reserves[0]
+    reserve1        = reserves[1]
 
     return f'${reserve1/reserve0}'
 
 def getTwinPrice(factory_address,factory_abi,factory_pair,InputTokenAddr,OutputTokenAddr) :
-    web3 = Web3(Web3.HTTPProvider('https://bsc-dataseed.binance.org/'))
-    pancake_factory_address = web3.toChecksumAddress(factory_address)
-    contract        = web3.eth.contract(address=pancake_factory_address, abi=factory_abi)
+    factory_address = web3.toChecksumAddress(factory_address)
+    contract        = web3.eth.contract(address=factory_address, abi=factory_abi)
     pair_address    = contract.functions.getPair(InputTokenAddr,OutputTokenAddr).call()
     pair1           = web3.eth.contract(abi=factory_pair, address=pair_address)
-    reserves = pair1.functions.getReserves().call()
-    reserve0 = reserves[0]
-    reserve1 = reserves[1]
+    reserves        = pair1.functions.getReserves().call()
+    reserve0        = reserves[0]
+    reserve1        = reserves[1]
 
     return f'${reserve1/reserve0}'
 
